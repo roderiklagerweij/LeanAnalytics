5f501c38d51662530a17ea707ab705475b56b9b3
= <=>
2019-03-15 15:23:03 +0100

    Added instrumentation test

---
+++ leananalytics/src/androidTest/java/com/rl/leananalytics/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.rl.leananalytics
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.test_activity_layout)
+    }
+}
---
+++ leananalytics/src/androidTest/java/com/rl/leananalytics/TrackActivityTest.kt
@@ -0,0 +1,39 @@
+package com.rl.leananalytics
+
+import android.app.Application
+import android.content.Intent
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+
+@RunWith(AndroidJUnit4::class)
+class TrackActivityTest {
+
+    @Rule
+    @JvmField
+    public val rule = ActivityTestRule(TestActivity::class.java, false, false)
+
+    val mockTrackPageAdapter = mock(TrackPageAdapter::class.java)
+
+    @Before
+    fun setup() {
+        LeanAnalyticsSdk.init(InstrumentationRegistry.getTargetContext().applicationContext as Application, mockTrackPageAdapter)
+    }
+
+    @Test
+    fun testTrackActivity() {
+        rule.launchActivity(Intent())
+        onView(withText("Test")).check(matches(isDisplayed()))
+
+        verify(mockTrackPageAdapter, times(1)).trackActivity("com.rl.leananalytics.TestActivity")
+    }
+}
