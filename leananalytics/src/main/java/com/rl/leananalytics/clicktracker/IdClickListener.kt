84bfb2b4ab3f68c7b45a6de30d506766b0b973cf
= <=>
2019-03-15 13:47:16 +0100

    Update gradle to 3.3 / 4.10.1

--- leananalytics/src/main/java/com/rl/leananalytics/clicktracker/IdClickListener.kt
+++ leananalytics/src/main/java/com/rl/leananalytics/clicktracker/IdClickListener.kt
@@ -2,7 +2,6 @@ package com.rl.leananalytics.clicktracker
 
 import android.util.Log
 import android.view.View
-import com.google.firebase.analytics.FirebaseAnalytics
 
 /**
  * Created by Roderik on 23-05-18.
@@ -12,9 +11,6 @@ class IdClickListener(private val listener : View.OnClickListener) : View.OnClic
     override fun onClick(view: View) {
         listener.onClick(view)
         val viewId = view.resources.getResourceEntryName(view.id)
-        Log.d("IdClickListener", "click for id: " + viewId)
-
-        val tracker = FirebaseAnalytics.getInstance(view.context)
-        tracker.logEvent(viewId, null)
+        Log.d("IdClickListener", "click for id: $viewId")
     }
 }
